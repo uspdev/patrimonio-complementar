c5b6d208b0d76211954d6fa7116de7d89f7ca61a
Masaki Kawabata Neto <kawabata@usp.br>
2021-11-18 07:51:58 -0300

    ajustando mensagem 403

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 
-  <h2 class="text-danger">
-    Acesso negado!!
-  </h2>
   @if (Gate::check('user'))
+    <h4 class="text-danger">
+      Acesso negado!!
+    </h4>
     Você não tem privilégios para acessar esse recurso.
   @else
     Faça <a href="{{ route('login') }}">login</a> para acessar esta página.
