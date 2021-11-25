1b4586c09efa6827deb7bec584f6b75594e5e277
Masaki Kawabata Neto <kawabata@usp.br>
2021-11-25 10:03:56 -0300

    liberando import

--- app/Models/Localusp.php
+++ app/Models/Localusp.php
@@ -11,7 +11,7 @@ class Localusp extends Model
     use HasFactory;
 
     protected $fillable = [
-        'codlocusp',
+        'codlocusp','setor','andar','nome'
     ];
 
     protected $casts = [
