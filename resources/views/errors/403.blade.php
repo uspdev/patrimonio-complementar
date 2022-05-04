9d719fd70bf9a0fc2270a587303a221988ac080c
Masaki Kawabata Neto <kawabata@usp.br>
2022-05-04 09:59:21 -0300

    melhorando mensagem de acesso

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-
+  <div class="h5 mb-4">
+    Gerencia dados complementares ao sistema patrimonial da USP.
+  </div>
   @if (Gate::check('user'))
     <h4 class="text-danger">
       Acesso negado!!
@@ -10,5 +12,4 @@
   @else
     Faça <a href="{{ route('login') }}">login</a> para acessar esta página.
   @endif
-
 @endsection
