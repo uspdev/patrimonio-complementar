8f56e57f247966741fa73941092e2252baf86903
Masaki K Neto <kawabata@usp.br>
2023-09-22 12:47:32 -0300

    testando novos status

--- resources/views/partials/badge-status.blade.php
+++ resources/views/partials/badge-status.blade.php
@@ -1,12 +1,24 @@
 @if ($patrimonio->conferido_em)
   @if ($patrimonio->temPendencias())
     <span class="d-none">1pendente</span>
-    <span class="badge badge-warning"><i class="fas fa-exclamation-triangle"></i></span>
+    <span class="badge badge-danger" title="Com pendências">
+      <i class="fas fa-exclamation-triangle"></i> {{ $patrimonio->conferido_em->diffInDays(now()) }}
+    </span>
   @else
     <span class="d-none">2conferido</span>
-    <span class="badge badge-success"><i class="fas fa-check"></i></span>
+    @if ($patrimonio->conferido_em->diffInDays(now()) > 90)
+      <span class="badge badge-warning" title="Verificado há mais de 90 dias">
+        <i class="fas fa-exclamation"></i> {{ $patrimonio->conferido_em->diffInDays(now()) }}
+      </span>
+    @else
+      <span class="badge badge-success" title="Verificado há menos de 90 dias">
+        <i class="fas fa-check"></i> {{ $patrimonio->conferido_em->diffInDays(now()) }}
+      </span>
+    @endif
   @endif
 @else
   <span class="d-none">0naoVerificado</span>
-  <span class="badge badge-secondary"><i class="fas fa-question"></i></span>
+  <span class="badge badge-secondary" title="Não verificado">
+    <i class="fas fa-question"></i>
+  </span>
 @endif
